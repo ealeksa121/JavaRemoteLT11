fc0b767192f60273539182be0606697124b0b65a
eivydasaleksa <ealeksa121@gmail.com>
2021-12-05 11:48:53 +0200

    Initial commit

---
+++ src/test/java/testingRecap/CalculatorTest.java
@@ -0,0 +1,16 @@
+package testingRecap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addMultipleNumbers() {
+    }
+}

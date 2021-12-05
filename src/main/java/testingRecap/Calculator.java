a578dbebe832c89ab29bc19bf7f124a98804991f
eivydasaleksa <ealeksa121@gmail.com>
2021-12-05 13:18:03 +0200

    Initial commit

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -2,17 +2,20 @@ package testingRecap;
 
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     private static final Logger LOGGER = Logger.getLogger(Calculator.class);
 
-    public int add(int firstNumber, int secondNumber){
+    public int add(int firstNumber, int secondNumber) {
         LOGGER.info("Adding two numbers " + firstNumber + " and " + secondNumber);
         return firstNumber + secondNumber;
 
     }
 
     public int addMultipleNumbers(int[] numbers) {
+        LOGGER.info("Adding numbers" + Arrays.toString(numbers));
         int total = 0;
         for (int number : numbers) {
             total += number;
@@ -20,5 +23,18 @@ public class Calculator {
         return total;
     }
 
+    public int subtractTwoNumbers(int firstNumber, int secondNumbers) {
+        LOGGER.info("Subtracting two numbers " + firstNumber + " and " + secondNumbers);
+        return firstNumber - secondNumbers;
+    }
 
+    public int subtractMultipleNumbers(int[] numbers) {
+        LOGGER.info("Subtracting numbers" + Arrays.toString(numbers));
+        int total = 0;
+        for (int number : numbers) {
+            total -= number;
+        }
+        return total;
+
+    }
 }
--- src/test/java/testingRecap/CalculatorTest.java
+++ src/test/java/testingRecap/CalculatorTest.java
@@ -2,15 +2,61 @@ package testingRecap;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
+    private Calculator calculator = new Calculator();
+
     @Test
-    void add() {
+    void testAddTwoNumbers() {
+
+        int firstNumber = 12;
+        int secondNumber = 3;
+
+        int actualAddResult = calculator.add(firstNumber, secondNumber);
+
+        assertEquals(15, actualAddResult);
+        assertThat(actualAddResult).isEqualTo(15);
+
+
     }
 
     @Test
-    void addMultipleNumbers() {
+    void testAddMultipleNumbers() {
+
+        int[] numbersToTest = {5, 2, -6, 8, 1};
+
+        int actualResult = calculator.addMultipleNumbers(numbersToTest);
+
+        assertEquals(10, actualResult);
+        assertThat(actualResult).isEqualTo(10);
+
+
+    }
+
+    @Test
+    void tesSubstractingTwoNumbers() {
+
+        int firstNumber = 5;
+        int secondNumber = 3;
+
+        int actualResult = calculator.subtractTwoNumbers(firstNumber, secondNumber);
+
+        assertThat(actualResult).isEqualTo(2);
+
+    }
+
+    @Test
+    void testSubtractMultipleNumbers() {
+
+        int[] numbersToTest = {5, 2, 6, 8, 1};
+
+        int actualResult = calculator.subtractMultipleNumbers(numbersToTest);
+
+        assertThat(actualResult).isEqualTo(-22);
+
+
     }
 }
